050739c74085e576efb0e3c82e1174a6f6a9a4e8
michaeloye <michaeloye23@gmail.com>
2023-08-17 14:07:17 +0100

    Implement Sort Array By Parity solution

---
+++ array/inPlaceOperations/SortArrayByParity.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+
+        slow = 0
+        fast = 1
+        length = len(nums)
+
+        while slow < length:
+            if (nums[slow] % 2 != 0):
+                while (nums[fast] % 2 != 0 and fast < length):
+                    fast += 1
+                    if (fast == length):
+                        return nums
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            slow += 1
+        return nums
+
+
+soln = Solution()
+nums = [3, 1, 2, 4]
+print(soln.sortArrayByParity(nums))
