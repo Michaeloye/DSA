f7e161da0a9c8e439723b60a1c8a2399e9079d96
michaeloye <michaeloye23@gmail.com>
2023-09-14 19:28:51 +0100

    Reverse a linked list

---
+++ linkList/singlyLinkedList/ReverseLinkedList.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from singlyLinkedList import ListNode
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+      prev = None
+      cur = head
+      stash = cur
+
+      while cur is not None:
+        stash = cur.next
+
+        cur.next = prev
+        prev = cur
+
+        cur = stash
+
+      return prev
