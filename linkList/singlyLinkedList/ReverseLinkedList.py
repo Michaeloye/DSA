70249914701e8e59a96462808db49f00f91be565
michaeloye <michaeloye23@gmail.com>
2023-10-06 15:07:35 +0100

    Format code

--- linkList/singlyLinkedList/ReverseLinkedList.py
+++ linkList/singlyLinkedList/ReverseLinkedList.py
@@ -6,18 +6,20 @@ from singlyLinkedList import ListNode
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-      prev = None
-      cur = head
-      stash = cur
+        prev = None
+        cur = head
+        stash = cur
 
-      while cur is not None:
-        stash = cur.next
+        while cur is not None:
+            stash = cur.next
 
-        cur.next = prev
-        prev = cur
+            cur.next = prev
+            prev = cur
 
-        cur = stash
+            cur = stash
 
-      return prev
+        return prev
