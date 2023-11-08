7887debfaac1cf5a474ccec6204cbc38e6c85bdd
michaeloye <michaeloye23@gmail.com>
2023-11-08 12:54:26 +0100

    Implement Solution multi level Doubly Linked List

---
+++ linkList/conclusion/FlattenMultilevelDoublyLinkedList.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        curr = head
+        stack = []
+
+        while curr:
+            if curr.child:
+                if curr.next:
+                    stack.append(curr.next)
+                curr.next = curr.child
+                curr.child.prev = curr
+                curr.child = None
+            else:
+                if not curr.next and len(stack):
+                    temp = stack.pop()
+                    temp.prev = curr
+                    curr.next = temp
+
+            curr = curr.next
+        return head
