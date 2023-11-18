630b3c1f7e5cfc78731953a7961bf1fb63414227
michaeloye <michaeloye23@gmail.com>
2023-11-18 23:13:27 +0100

    Add link to problem

--- neetcodeAll/arraysAndHashing/ReplaceElementsWithGreatest.py
+++ neetcodeAll/arraysAndHashing/ReplaceElementsWithGreatest.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side
+
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         length = len(arr)
