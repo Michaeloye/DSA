001878c466c2c7c9b93b2844e9b169b816e2fe4e
michaeloye <michaeloye23@gmail.com>
2023-11-22 23:10:06 +0100

    Implement Remove Element solution

---
+++ neetcodeAll/arraysAndHashing/RemoveElement.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/remove-element
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = 0
+
+        for fast in nums:
+            if fast != val:
+                nums[slow] = fast
+                slow += 1
+
+        return slow
