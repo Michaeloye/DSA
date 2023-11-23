14a09b0a7f5265bb2e63f385b4e8ca09d38717f6
michaeloye <michaeloye23@gmail.com>
2023-11-23 20:35:37 +0100

    Add another solution

--- neetcodeAll/arraysAndHashing/UniqueEmailAddress.py
+++ neetcodeAll/arraysAndHashing/UniqueEmailAddress.py
@@ -17,3 +17,32 @@ class Solution:
             validEmails.add(parsedEmail)
 
         return len(validEmails)
+
+
+# Another solution
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        def isValidEmail(email):
+            splitEmail = email.split("@")
+            localName = splitEmail[0]
+            domainName = splitEmail[1]
+
+            verifiedLocalName = ""
+
+            for i in localName:
+                if i == '+':
+                    break
+                if i != '.':
+                    verifiedLocalName += i
+            verifiedEmail = f"{verifiedLocalName}@{domainName}"
+            return verifiedEmail
+
+        validEmails = set()
+
+        for i in emails:
+
+            if isValidEmail(i):
+                validEmails.add(isValidEmail(i))
+
+        return len(validEmails)
