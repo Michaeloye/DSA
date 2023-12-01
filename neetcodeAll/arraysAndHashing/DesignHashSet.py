a4477e0f2d76e2b0b5e7a212a7df3f06c208b942
michaeloye <michaeloye23@gmail.com>
2023-12-01 14:56:36 +0100

    Optimize design hash set solution

--- neetcodeAll/arraysAndHashing/DesignHashSet.py
+++ neetcodeAll/arraysAndHashing/DesignHashSet.py
@@ -82,3 +82,54 @@ class MyHashSet:
 # obj.add(key)
 # obj.remove(key)
 # param_3 = obj.contains(key)
+
+# optimized solution, using linked list
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+
+class MyHashSet:
+
+    def __init__(self):
+        self.space = 769
+        self.set = [ListNode(0) for _ in range(self.space)]
+
+    def _hash(self, key):
+        return key % self.space
+
+    def add(self, key: int) -> None:
+        curr = self.set[self._hash(key)]
+
+        while curr and curr.next:
+            if curr.next.key == key:
+                return
+            curr = curr.next
+
+        curr.next = ListNode(key)
+
+    def remove(self, key: int) -> None:
+        curr = self.set[self._hash(key)]
+
+        while curr and curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+            curr = curr.next
+
+    def contains(self, key: int) -> bool:
+        curr = self.set[self._hash(key)]
+
+        while curr and curr.next:
+            if curr.next.key == key:
+                return True
+            curr = curr.next
+
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
