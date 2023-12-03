943f48563528fee170109a1d03499ca7cbedbd56
michaeloye <michaeloye23@gmail.com>
2023-12-03 15:29:53 +0100

    Implement Quick sort

---
+++ sort/QuickSort.py
@@ -0,0 +1,27 @@
+
+def QuickSort(array, start, end):
+    if (end <= start):
+        return
+
+    print(array, start, end)
+    pivot = array[end]
+
+    i = start - 1
+
+    for j in range(start, end + 1):
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    i += 1
+
+    array[i], array[end] = array[end], array[i]
+
+    QuickSort(array, start, i - 1)
+    QuickSort(array, i + 1, end)
+
+
+arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
+
+QuickSort(arr, 0, len(arr) - 1)
+print(arr)
