9e064944506dba4d05d6222fe1f9c507babc266f
michaeloye <michaeloye23@gmail.com>
2023-12-06 01:16:53 +0100

    Add another top k frequent solution

--- neetcodeAll/arraysAndHashing/TopKFrequentElements.py
+++ neetcodeAll/arraysAndHashing/TopKFrequentElements.py
@@ -28,3 +28,24 @@ class Solution:
 
         del hashMap[maxkey]
         return maxkey
+
+
+# buckek approach
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        bucketList = [[] for _ in range(len(nums) + 1)]
+        numsCountMap = {}
+        res = []
+
+        for i in nums:
+            numsCountMap[i] = 1 + numsCountMap.get(i, 0)
+
+        for key in numsCountMap:
+            bucketList[numsCountMap[key]].append(key)
+
+        for i in range(len(bucketList) - 1, 0, -1):
+            for j in bucketList[i]:
+                if k > 0:
+                    res.append(j)
+                    k -= 1
+        return res
