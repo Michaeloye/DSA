01bd83ef5beaed78f73c5aa8cc1658091eb6dc39
michaeloye <michaeloye23@gmail.com>
2024-01-02 16:11:21 +0100

    Add second solution

--- neetcodeAll/arraysAndHashing/InsertDeleteGetRandomO1.py
+++ neetcodeAll/arraysAndHashing/InsertDeleteGetRandomO1.py
@@ -52,3 +52,44 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+# SECOND SOLUTION
+class RandomizedSet:
+
+    def __init__(self):
+        self.hashMap = {}
+        self.numArray = []
+
+    def insert(self, val: int) -> bool:
+        res = val in self.hashMap
+
+        if not res:
+            self.numArray.append(val)
+            self.hashMap[val] = len(self.numArray) - 1
+
+        return not res
+
+    def remove(self, val: int) -> bool:
+        res = val in self.hashMap
+
+        if res:
+            idx = self.hashMap[val]
+            lastItem = self.numArray[-1]
+            self.numArray[idx] = lastItem
+            self.numArray.pop()
+
+            self.hashMap[lastItem] = idx
+            del self.hashMap[val]
+        return res
+
+    def getRandom(self) -> int:
+        randomNum = random.randint(0, len(self.numArray) - 1)
+
+        return self.numArray[randomNum]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
