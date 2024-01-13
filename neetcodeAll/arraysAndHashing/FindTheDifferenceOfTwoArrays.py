9d7803f4af4b930ef4ecbeba33589b2381bfe098
michaeloye <michaeloye23@gmail.com>
2024-01-13 16:17:56 +0100

    Solve find the Difference of Two Arrays

---
+++ neetcodeAll/arraysAndHashing/FindTheDifferenceOfTwoArrays.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        answer = [[], []]
+
+        answer[0] = list(nums1.difference(nums2))
+        answer[1] = list(nums2.difference(nums1))
+        return answer
