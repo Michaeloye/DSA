68fea8af9f1decd7d8a8ccfece7ffedb7d6be4eb
michaeloye <michaeloye23@gmail.com>
2024-01-19 20:13:50 +0100

    Add DP solution

--- leetcodeDailyChallenge/MinimumFallingPathSum.py
+++ leetcodeDailyChallenge/MinimumFallingPathSum.py
@@ -26,3 +26,19 @@ class Solution:
             minVal = min(minVal, dfs(0, i))
 
         return minVal
+
+# DP SOLUTION
+
+
+def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+    N = len(matrix)
+
+    for r in range(1, N):
+        for c in range(N):
+            left = matrix[r - 1][c - 1] if c > 0 else float("inf")
+            mid = matrix[r - 1][c]
+            right = matrix[r - 1][c + 1] if c < N - 1 else float("inf")
+
+            matrix[r][c] = matrix[r][c] + min(left, mid, right)
+
+    return min(matrix[-1])
