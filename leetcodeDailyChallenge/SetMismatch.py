0708ec1ec1267b564acf30c2f1f3f86bc4a4732d
michaeloye <michaeloye23@gmail.com>
2024-01-22 12:23:06 +0100

    Add other solutions

--- leetcodeDailyChallenge/SetMismatch.py
+++ leetcodeDailyChallenge/SetMismatch.py
@@ -21,3 +21,69 @@ class Solution:
             uniqueNums.add(val)
 
         return ans
+
+# second solution
+
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0, 0]
+        duplicate = 0
+
+        for i in range(n):
+            idx = abs(nums[i])
+            idx = idx - 1
+            if nums[idx] < 0:
+                duplicate = abs(nums[i])
+                ans[0] = duplicate
+            nums[idx] *= -1
+
+        for i in range(n):
+            if nums[i] > 0 and i + 1 != duplicate:
+                ans[1] = i + 1
+        return ans
+
+
+# third solution
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        x = 0  # duplicate - missing
+        y = 0  # duplicate^2 - missing^2
+
+        for i in range(1, n + 1):
+            x += nums[i - 1] - i
+            y += nums[i - 1]**2 - i ** 2
+
+        missing = (y - x**2) // (2 * x)
+        duplicate = (missing) + x
+
+        return [duplicate, missing]
+
+
+# four solution using AP
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        expectedSum = n * (n + 1) // 2
+
+        seen = set()
+        duplicate = 0
+        missing = 0
+
+        actualSum = 0
+
+        for num in nums:
+            if num in seen:
+                duplicate = num
+
+            seen.add(num)
+            actualSum += num
+
+        missing = expectedSum - actualSum + duplicate
+
+        return [duplicate, missing]
