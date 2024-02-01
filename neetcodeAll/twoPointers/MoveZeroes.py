fe367e4027631f5718dc05f3d85100000be94a10
michaeloye <michaeloye23@gmail.com>
2024-02-01 12:00:04 +0100

    Add Optimized solution

--- neetcodeAll/twoPointers/MoveZeroes.py
+++ neetcodeAll/twoPointers/MoveZeroes.py
@@ -13,3 +13,17 @@ class Solution:
                     continue
                 nums[i], nums[j] = nums[j], nums[i]
                 break
+
+
+# OPTIMIZED SOLUTION
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
