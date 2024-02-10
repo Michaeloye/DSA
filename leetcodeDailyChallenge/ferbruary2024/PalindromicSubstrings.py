0d6723c7517f482b7718b1a61f09ea8acd33fb6d
michaeloye <michaeloye23@gmail.com>
2024-02-10 17:00:53 +0100

    Add optimized code

--- leetcodeDailyChallenge/ferbruary2024/PalindromicSubstrings.py
+++ leetcodeDailyChallenge/ferbruary2024/PalindromicSubstrings.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/palindromic-substrings/
 
+# O(n^3)
 class Solution:
     def countSubstrings(self, s: str) -> int:
         ans = len(s)
@@ -11,4 +12,31 @@ class Solution:
                 if subStr == subStr[::-1]:
                     ans += 1
         
+        return ans
+
+# optimized O(n^2)
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        ans = len(s)
+
+        # for odd length palindromes
+        for i in range(len(s)):
+            if i > 0 and i < len(s):
+                l = i - 1
+                r = i + 1
+
+                while l >= 0 and r < len(s) and s[l] == s[r]:
+                    ans += 1
+                    l -= 1
+                    r += 1
+            
+        # for even length palindromes
+        for i in range(len(s) ):
+            l = i
+            r = l + 1
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                ans += 1
+                l -= 1
+                r += 1
         return ans
