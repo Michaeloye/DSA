853e7f09ac9211fb189b2a857416e1eb4bbe1acc
michaeloye <michaeloye23@gmail.com>
2024-02-14 00:51:11 +0100

    Add leetcode problem

--- recursion/youtubeTutorial/10SubsetSum.py
+++ recursion/youtubeTutorial/10SubsetSum.py
@@ -18,3 +18,25 @@ def subsetSum(idx, currSum):
 subsetSum(0, 0)
 ans.sort()
 print(ans)
+
+# https://leetcode.com/problems/subsets
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def uniqueSet(idx, subset):
+            if idx == len(nums):
+                ans.append([*subset])
+                return
+
+            # pick
+            subset.append(nums[idx])
+            uniqueSet(idx + 1, subset)
+
+            # don't pick
+            subset.pop()
+            uniqueSet(idx + 1, subset)
+
+        uniqueSet(0, [])
+        return ans
