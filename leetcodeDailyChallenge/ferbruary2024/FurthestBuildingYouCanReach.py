abd88325cbfcdff81b87f21360eccc8a061629a7
michaeloye <michaeloye23@gmail.com>
2024-02-17 12:21:55 +0100

    Solve using heap ds

--- leetcodeDailyChallenge/ferbruary2024/FurthestBuildingYouCanReach.py
+++ leetcodeDailyChallenge/ferbruary2024/FurthestBuildingYouCanReach.py
@@ -21,3 +21,26 @@ class Solution:
                 ladders -= 1
 
         return len(heights) - 1
+
+# using heap
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+
+        for i in range(len(heights) -1 ):
+            diff = heights[i + 1] - heights[i]
+
+            if diff <= 0:
+                continue
+            
+            bricks -= diff
+            heapq.heappush(heap, -diff)
+
+            if bricks < 0:
+                if ladders == 0:
+                    return i
+
+                bricks += -heapq.heappop(heap)
+                ladders -= 1
+        
+        return len(heights) - 1
