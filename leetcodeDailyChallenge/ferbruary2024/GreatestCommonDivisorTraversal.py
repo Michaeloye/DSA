c08eec03575bab976366413f8b9730481abc1067
michaeloye <michaeloye23@gmail.com>
2024-02-25 23:49:37 +0100

    Add another solution

--- leetcodeDailyChallenge/ferbruary2024/GreatestCommonDivisorTraversal.py
+++ leetcodeDailyChallenge/ferbruary2024/GreatestCommonDivisorTraversal.py
@@ -48,3 +48,24 @@ class Solution:
                     factorIdx[n] = i
 
         return uf.count == 1
+
+
+# shorter solution
+# https://leetcode.com/problems/greatest-common-divisor-traversal/submissions/1186187041/
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        if len(nums)==1:return True
+        nums = set(nums)
+        if 1 in nums:return False
+        if len(nums)==1:return True
+        
+        nums = sorted(nums,reverse=True)
+
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if gcd(nums[i],nums[j])-1:
+                    nums[j]*=nums[i]
+                    break
+            else:
+                return False
+        return True
