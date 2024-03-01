87cd409e2857857cf3f8181dd4b282b04e8540aa
michaeloye <michaeloye23@gmail.com>
2024-03-01 21:55:10 +0100

    Add another solution

--- leetcodeDailyChallenge/march2024/MaximumOddBinaryNumber.py
+++ leetcodeDailyChallenge/march2024/MaximumOddBinaryNumber.py
@@ -1,5 +1,22 @@
 # https://leetcode.com/problems/maximum-odd-binary-number/
 
+
+# another solution
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+
+        el = [c for c in s]
+        l = 0
+
+        for i in range(len(el)):
+            if el[i] == "1":
+                el[l], el[i] = el[i], el[l]
+                l += 1
+
+        el[l - 1], el[-1] = el[-1], el[l - 1]
+        return "".join(el)
+
+
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
         count = 0
