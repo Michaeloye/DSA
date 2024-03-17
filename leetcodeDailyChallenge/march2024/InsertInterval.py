436333cf2bbbe40a5807fa281276514c78023e3c
michaeloye <michaeloye23@gmail.com>
2024-03-17 12:58:10 +0100

    Solve insert interval

---
+++ leetcodeDailyChallenge/march2024/InsertInterval.py
@@ -0,0 +1,49 @@
+# https://leetcode.com/problems/insert-interval/description/
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        intersections = []
+        indexes = []
+
+        # get interval where intersections occured
+        for i in range(len(intervals)):
+            start, end = newInterval
+            a, b = intervals[i]
+
+            if start in range(a, b + 1) or a in range(start, end + 1) or b in range(start, end + 1) or end in range(a, b + 1):
+                intersections.append([a, b])
+                indexes.append(i)
+        
+        # handle when there is no intersection
+        if len(intersections) == 0:
+            start, end = newInterval
+            a, _ = intervals[0] if len(intervals) else [-1, -1]
+
+            if a < start:
+                intervals.append(newInterval)
+            else:
+                intervals.insert(0, newInterval)
+
+            intervals.sort()
+            return intervals
+
+        # handle when there is intersection
+        a, _ = intersections[0]
+        _, b = intersections[-1]
+        start, end = newInterval
+        newStart = a if a <= start else start
+        newEnd = b if b >= end else end
+        
+        i = 0
+
+        while i < len(intervals):
+            a, b = intervals[i]
+            if i not in indexes:
+                res.append([a, b])
+            elif i in indexes and indexes[0] == i:
+                res.append([newStart, newEnd])
+                i += len(indexes)
+                continue
+            i += 1
+        return res
