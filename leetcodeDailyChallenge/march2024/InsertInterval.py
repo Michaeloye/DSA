163e7781b783714c5bae95bb83be9ebd90a9e77d
michaeloye <michaeloye23@gmail.com>
2024-03-17 12:58:37 +0100

    Add optimized solution

--- leetcodeDailyChallenge/march2024/InsertInterval.py
+++ leetcodeDailyChallenge/march2024/InsertInterval.py
@@ -47,3 +47,21 @@ class Solution:
                 continue
             i += 1
         return res
+
+# optimized solution
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < newInterval[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > newInterval[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        
+        res.append(newInterval)
+
+        return res
