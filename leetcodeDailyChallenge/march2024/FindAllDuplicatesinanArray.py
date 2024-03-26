6f6e15b690005ce85ad78c58bcf567974e2a6382
michaeloye <michaeloye23@gmail.com>
2024-03-26 01:35:38 +0100

    Solve find All Duplicates in an Array

---
+++ leetcodeDailyChallenge/march2024/FindAllDuplicatesinanArray.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-all-duplicates-in-an-array
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for n in nums:
+            n = abs(n)
+            if nums[n - 1] < 0:
+                res.append(n)
+            nums[n - 1] *= -1
+        return res
