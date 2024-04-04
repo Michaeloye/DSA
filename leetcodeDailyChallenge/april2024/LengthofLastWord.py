9ff659570bc3ee48855347d8a4bbfb25c5a34c6a
michaeloye <michaeloye23@gmail.com>
2024-04-04 16:03:55 +0100

    Solve length of last word

---
+++ leetcodeDailyChallenge/april2024/LengthofLastWord.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/length-of-last-word/
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        for i in s[::-1]:
+            if i != " ":
+                count += 1
+            if i == " " and count:
+                return count
+
+        return count
