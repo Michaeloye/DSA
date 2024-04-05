c2d6935785000c885aa9a557d515b8ce36e4ca41
michaeloye <michaeloye23@gmail.com>
2024-04-05 16:53:38 +0100

    Add linear time solution

--- leetcodeDailyChallenge/april2024/MakeTheStringGreat.py
+++ leetcodeDailyChallenge/april2024/MakeTheStringGreat.py
@@ -22,3 +22,19 @@ class Solution:
             return newStr  # Return newStr if no modifications were made
 
         return "".join(handler(list(s)))
+
+# linear time solution
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+
+        i = 0
+        while i < len(s):
+            
+            if (stack and (stack[-1] != s[i]) and stack[-1].lower() == s[i].lower()):
+                stack.pop()
+            else:
+                stack.append(s[i])
+            i += 1
+
+        return ''.join(stack)
