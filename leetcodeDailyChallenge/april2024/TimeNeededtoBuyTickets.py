82a8151c001a22cf8929e534c5a83934071b9999
michaeloye <michaeloye23@gmail.com>
2024-04-10 16:17:16 +0100

    Solve time Needed to Buy Tickets

--- leetcodeDailyChallenge/april2024/TimeNeededtoBuyTickets.py
+++ leetcodeDailyChallenge/april2024/TimeNeededtoBuyTickets.py
@@ -1,2 +1,13 @@
 # https://leetcode.com/problems/time-needed-to-buy-tickets/
 
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        ans = 0
+
+        for i in range(len(tickets)):
+            if i <= k:
+                ans += min(tickets[i], tickets[k])
+            else:
+                ans += min(tickets[i], tickets[k] - 1)
+
+        return ans
