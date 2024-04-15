529eca0089d48683232a147bf71f4180ec366658
michaeloye <michaeloye23@gmail.com>
2024-04-15 21:21:35 +0100

    Add O(1) space solution

--- leetcodeDailyChallenge/april2024/TrappingRainWater.py
+++ leetcodeDailyChallenge/april2024/TrappingRainWater.py
@@ -26,3 +26,27 @@ class Solution:
             ans += diff if diff >=0 else 0
 
         return ans
+
+# O(1) space
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+
+        maxLeft = height[l]
+        maxRight = height[r]
+
+        res = 0
+
+        while l < r:
+            if maxLeft < maxRight:
+                diff = maxLeft - height[l]
+                res += diff if diff >= 0 else 0
+                l += 1
+                maxLeft = max(maxLeft, height[l])
+            else:
+                diff = maxRight - height[r]
+                res += diff if diff >= 0 else 0
+                r -= 1
+                maxRight = max(maxRight, height[r])
+        return res
