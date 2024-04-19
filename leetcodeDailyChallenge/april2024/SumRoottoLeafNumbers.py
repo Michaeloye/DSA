6e08b78224f24e48c54536c9e0e424c186b7aebd
michaeloye <michaeloye23@gmail.com>
2024-04-20 00:29:21 +0100

    Add another solution

--- leetcodeDailyChallenge/april2024/SumRoottoLeafNumbers.py
+++ leetcodeDailyChallenge/april2024/SumRoottoLeafNumbers.py
@@ -33,3 +33,27 @@ class Solution:
 
 
         return totalSum
+
+# Another solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node, currSum):
+            if not node:
+                return 0
+            
+            currSum = currSum * 10 + node.val
+
+            if not node.left and not node.right:
+                return currSum
+            else:
+                return dfs(node.left, currSum) + dfs(node.right, currSum)
+        
+        return dfs(root, 0)
