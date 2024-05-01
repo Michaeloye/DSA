c10bbff102eaaed457fb99581aab24f424adffe8
michaeloye <michaeloye23@gmail.com>
2024-05-01 22:13:18 +0100

    Add another solution

--- neetcodeAll/twoPointers/ValidPalindrome.py
+++ neetcodeAll/twoPointers/ValidPalindrome.py
@@ -16,3 +16,28 @@ class Solution:
             lowerS += tmp
 
         return lowerS == lowerS[::-1]
+
+
+# add another solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        s = s.lower()
+        s = s.replace(" ", "")
+        r = len(s) - 1
+
+        if len(s) == 1:
+            return True
+
+        while l <= r:
+            while l <= r and not (ord(s[l]) in range(ord('a'), ord('z') + 1) or ord(s[l]) in range(ord('0'), ord('9') + 1)):
+                l += 1
+            while l <= r and not (ord(s[r]) in range(ord('a'), ord('z') + 1) or ord(s[r]) in range(ord('0'), ord('9') + 1)):
+                r -= 1
+
+            if l <= r and s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
