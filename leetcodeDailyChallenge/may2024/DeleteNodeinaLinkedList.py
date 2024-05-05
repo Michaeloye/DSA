b0e6584d2a158b407ee77eaf86229b8d79b766cc
michaeloye <michaeloye23@gmail.com>
2024-05-05 18:04:30 +0100

    Solve delete Node in a Linked List

---
+++ leetcodeDailyChallenge/may2024/DeleteNodeinaLinkedList.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+
+        # change current node value to the next node's value
+        # then point current node next to the next node's next
+        node.val = node.next.val
+        node.next =node.next.next
