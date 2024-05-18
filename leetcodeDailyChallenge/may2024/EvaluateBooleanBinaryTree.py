df72a0ba5f616587374eff4e45f1b0d41c1157d9
michaeloye <michaeloye23@gmail.com>
2024-05-18 17:09:03 +0100

    Add iterative solution

--- leetcodeDailyChallenge/may2024/EvaluateBooleanBinaryTree.py
+++ leetcodeDailyChallenge/may2024/EvaluateBooleanBinaryTree.py
@@ -16,3 +16,27 @@ class Solution:
 
         if root.val == 3:
             return self.evaluateTree(root.left) and self.evaluateTree(root.right)
+
+# iterative* solution
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        stack = [root]
+        value = {} # node: value
+
+        while stack:
+            node = stack.pop()
+
+            if not node.left:
+                value[node] = node.val == 1
+
+            else:
+                # node in value
+                if node.left in value:
+                    if node.val == 2:
+                        value[node] = value[node.left] or value[node.right]
+                    elif node.val == 3:
+                        value[node] = value[node.left] and value[node.right]
+                else:
+                    stack.extend([node, node.left, node.right])
+
+        return value[root]
