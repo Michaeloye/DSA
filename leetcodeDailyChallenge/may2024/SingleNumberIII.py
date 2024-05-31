057f3d33c2fd53e1c66cec0eb3f7475945a1e5cf
michaeloye <michaeloye23@gmail.com>
2024-05-31 22:04:28 +0100

    Solve single Number III

---
+++ leetcodeDailyChallenge/may2024/SingleNumberIII.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/single-number-iii/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+
+        for n in nums:
+            xor ^= n
+        
+        diff_bit = 1
+
+        while not(diff_bit & xor):
+            diff_bit = diff_bit << 1
+
+        a = 0
+        b = 0
+
+        for n in nums:
+            if diff_bit & n:
+                a = a ^ n
+            else:
+                b = b ^ n
+
+        return [a, b]
