2aecd4ead487c634a87af20fc85f640b3b499640
michaeloye <michaeloye23@gmail.com>
2024-06-01 22:20:42 +0100

    Solve score of a string

---
+++ leetcodeDailyChallenge/june2024/ScoreofaString.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/score-of-a-string/
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ans = 0
+
+        for i in range(len(s) - 1):
+            ans += abs(ord(s[i]) - ord(s[i + 1]))
+
+        return ans
