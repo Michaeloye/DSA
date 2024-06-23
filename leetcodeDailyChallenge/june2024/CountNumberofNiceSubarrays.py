1b5acd133fa3e01fdcd35587718ae1ce7967013f
michaeloye <michaeloye23@gmail.com>
2024-06-23 13:36:44 +0100

    Solve count Number of Nice Subarrays

---
+++ leetcodeDailyChallenge/june2024/CountNumberofNiceSubarrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        l = 0
+        m = 0
+        oddCount = 0
+        res = 0
+
+        for r in range(len(nums)):
+            if nums[r] % 2 == 1:
+                oddCount += 1
+            
+            while oddCount > k:
+                if nums[l] % 2 == 1:
+                    oddCount -= 1
+
+                l += 1
+                m = l
+            if oddCount == k:
+                while nums[m] % 2 == 0:
+                    m += 1
+                res += m - l + 1
+
+        return res
