0dd983deed53e19cf2a7ca9a5c736666065c247b
michaeloye <michaeloye23@gmail.com>
2024-08-03 14:17:50 +0100

    Add another solution

--- leetcodeDailyChallenge/august2024/MakeTwoArraysEqualbyReversingSubarrays.py
+++ leetcodeDailyChallenge/august2024/MakeTwoArraysEqualbyReversingSubarrays.py
@@ -12,3 +12,18 @@ class Solution:
             arrCount[b] = 1 + arrCount.get(b, 0)
 
         return targetCount == arrCount
+
+
+# add another solution
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        count = defaultdict(int)
+        
+        for n1, n2 in zip(target, arr):
+            count[n1] += 1
+            count[n2] -= 1
+
+        for n in count:
+            if count[n] != 0:
+                return False
+        return True
