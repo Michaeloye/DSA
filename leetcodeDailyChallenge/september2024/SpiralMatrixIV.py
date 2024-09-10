e964d21d875e9057eaa16d86a48a6a646a891676
michaeloye <michaeloye23@gmail.com>
2024-09-10 13:04:37 +0100

    Optimize code

--- leetcodeDailyChallenge/september2024/SpiralMatrixIV.py
+++ leetcodeDailyChallenge/september2024/SpiralMatrixIV.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/spiral-matrix-iv/
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -10,18 +12,14 @@ class Solution:
 
         matrix = [[-1 for _ in range(n)] for _ in range(m)]
         direction = "r" # r, d, l, u (right, down, left, up)
-        
+
         r = 0
         c = 0
-
-        visited = set([(0, 0)])
-        total = m * n -1
-
         matrix[r][c] = head.val
 
-        while total >= 0:
+        while head:
             if direction == "r":
-                if c == COLS - 1 or (r, c + 1) in visited:
+                if c == COLS - 1 or matrix[r][c + 1] > -1:
                     direction = "d"
                 else:
                     c += 1
@@ -31,7 +29,7 @@ class Solution:
                     matrix[r][c] = head.val
 
             if direction == "d":
-                if r == ROWS - 1 or (r + 1, c) in visited:
+                if r == ROWS - 1 or matrix[r + 1][c] > -1:
                     direction = "l"
                 else:
                     r += 1
@@ -41,7 +39,7 @@ class Solution:
                     matrix[r][c] = head.val
 
             if direction == "l":
-                if c == 0 or (r, c - 1) in visited:
+                if c == 0 or matrix[r][c - 1] > -1:
                     direction = "u"
                 else:
                     c -= 1
@@ -50,11 +48,9 @@ class Solution:
                         return matrix
                     matrix[r][c] = head.val
             
-            # direction "u" is a 'special' case because when we want to transition from up to right, 
-            # we need to update our c pointer to point to the next column, 
-            # since the loop re-iterates after we handle direction "u" same reason for head
+            # direction "u" is a 'special' case because when we want to transition from up to right, we need to update our c pointer to point to the next column, since the loop re-iterates after we handle direction "u" same reason for head
             if direction == "u":
-                if r == 0 or (r - 1, c) in visited:
+                if r == 0 or matrix[r - 1][c] > -1:
                     direction = "r"
                     c += 1
                     head = head.next
@@ -66,7 +62,5 @@ class Solution:
                     return matrix
 
                 matrix[r][c] = head.val
-
-            visited.add((r, c))
-            total -= 1
         return matrix
+
