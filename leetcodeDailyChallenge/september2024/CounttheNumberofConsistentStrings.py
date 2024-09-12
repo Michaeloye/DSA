f2904937bbc9ebf626cec06bfae0d014e5df5ac9
michaeloye <michaeloye23@gmail.com>
2024-09-12 15:10:20 +0100

    Solve using bitwise operation

--- leetcodeDailyChallenge/september2024/CounttheNumberofConsistentStrings.py
+++ leetcodeDailyChallenge/september2024/CounttheNumberofConsistentStrings.py
@@ -16,3 +16,22 @@ class Solution:
                 ans += 1
         
         return ans
+
+# bitwise solution
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        bitwise = 0
+
+        for c in allowed:
+            bit = 1 << (ord(c) - ord('a'))
+            bitwise = bitwise | bit
+
+        ans = len(words)
+        for word in words:
+            for c in word:
+                bit = 1 << (ord(c) - ord('a'))
+                if bit & bitwise == 0:
+                    ans -= 1
+                    break
+
+        return ans
