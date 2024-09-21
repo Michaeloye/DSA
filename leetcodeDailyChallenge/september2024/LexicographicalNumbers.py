0d84aea9741525d7593df6d7f04ac68560a3be3d
michaeloye <michaeloye23@gmail.com>
2024-09-21 22:12:49 +0100

    Solve lexicographical Numbers

---
+++ leetcodeDailyChallenge/september2024/LexicographicalNumbers.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/lexicographical-numbers/
+
+# recursive
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+
+        def dfs(cur):
+            if cur > n:
+                return
+            
+            res.append(cur)
+            cur = cur * 10
+
+            for i in range(10):
+                dfs(cur + i)
+        
+        for i in range(1, 10):
+            dfs(i)
+
+        return res
