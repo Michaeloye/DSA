4e2d5da3bdaa4fe003e9ab0abf51998e2f33afe6
michaeloye <michaeloye23@gmail.com>
2024-10-05 20:46:17 +0100

    Add another solution

--- leetcodeDailyChallenge/october2024/PermutationinString.py
+++ leetcodeDailyChallenge/october2024/PermutationinString.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/permutation-in-string/
 
+# O(26 * n)
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
@@ -28,3 +29,52 @@ class Solution:
                 s2Map[s2[r]] = 1 + s2Map.get(s2[r], 0)
 
         return False
+
+
+# O(n) solution
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        s1Map = {}
+        s2Map = {}
+
+        for i in range(26):
+            char = chr(i + ord('a'))
+            s1Map[char] = 0
+            s2Map[char] = 0
+
+        for i in range(len(s1)):
+            letter1 = s1[i]
+            s1Map[letter1] += 1
+            letter2 = s2[i]
+            s2Map[letter2] += 1
+        
+        matches = 0
+        # check matches
+        for i in range(26):
+            letter = chr(i + ord('a'))
+            if s1Map[letter] == s2Map[letter]:
+                matches += 1
+
+        l = 0
+        
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+
+            s2Map[s2[r]] += 1
+            if s1Map[s2[r]] == s2Map[s2[r]]:
+                matches += 1
+            elif s1Map[s2[r]] + 1 == s2Map[s2[r]]:
+                matches -= 1
+
+            s2Map[s2[l]] -= 1
+
+            if s1Map[s2[l]] == s2Map[s2[l]]:
+                matches += 1
+            elif s1Map[s2[l]] - 1 == s2Map[s2[l]]:
+                matches -= 1
+
+            l += 1
+
+        return matches == 26
