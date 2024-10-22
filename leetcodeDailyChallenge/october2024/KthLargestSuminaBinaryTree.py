46dd4ab52f7407699d045dc0e435731a7f56f630
michaeloye <michaeloye23@gmail.com>
2024-10-22 23:04:17 +0100

    Solution using heap

--- leetcodeDailyChallenge/october2024/KthLargestSuminaBinaryTree.py
+++ leetcodeDailyChallenge/october2024/KthLargestSuminaBinaryTree.py
@@ -27,3 +27,34 @@ class Solution:
         for i in range(len(sums)):
             if i + 1 == k:
                 return sums[i]
+
+# Another solution using Heap
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        
+        queue = deque([root])
+        minHeap = []
+
+        while queue:
+            levelSum = 0
+
+            for i in range(len(queue)):
+                cur = queue.popleft()
+                levelSum += cur.val
+
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+
+            heapq.heappush(minHeap, levelSum)
+
+            while len(minHeap) > k:
+                heapq.heappop(minHeap)
+        return -1 if len(minHeap) < k else minHeap[0]
