277913d2213f833cffc53bc695a486fdf6edddc0
michaeloye <michaeloye23@gmail.com>
2024-10-22 22:11:35 +0100

    Solve Kth Largest Sum in a Binary Tree

---
+++ leetcodeDailyChallenge/october2024/KthLargestSuminaBinaryTree.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        
+        queue = deque([(root, 0)]) # (node, level)
+        sumMap = {}
+        while queue:
+            cur, level = queue.popleft()
+            sumMap[level] = cur.val + sumMap.get(level, 0)
+            if cur.left:
+                queue.append((cur.left, level + 1))
+            if cur.right:
+                queue.append((cur.right, level + 1))
+
+        sums= list(sumMap.values())
+        sums.sort(reverse=True)
+
+        if k > len(sums):
+            return -1
+        for i in range(len(sums)):
+            if i + 1 == k:
+                return sums[i]
