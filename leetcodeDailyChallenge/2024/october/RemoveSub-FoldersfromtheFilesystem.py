b7a12024f63e67150b417b2b4f79f295f1740649
michaeloye <michaeloye23@gmail.com>
2024-10-25 20:12:31 +0100

    Solve using Trie

--- leetcodeDailyChallenge/2024/october/RemoveSub-FoldersfromtheFilesystem.py
+++ leetcodeDailyChallenge/2024/october/RemoveSub-FoldersfromtheFilesystem.py
@@ -13,3 +13,47 @@ class Solution:
                     res.pop()
                     break
         return res
+
+
+# Trie Solution
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, path):
+        cur = self.root
+
+        for p in path.split('/'):
+            if p not in cur.children:
+                cur.children[p] = TrieNode()
+            cur = cur.children[p]
+        cur.end = True
+
+    def checkIfSubfolder(self, path):
+        cur = self.root
+        pathSplit = path.split("/")
+
+        for i in range(len(pathSplit) - 1):
+            cur = cur.children[pathSplit[i]]
+            if cur.end:
+                return True
+        return False
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        trie = Trie()
+        res = []
+
+        for f in folder:
+            trie.insert(f)
+        
+        for f in folder:
+            if not trie.checkIfSubfolder(f):
+                res.append(f)
+        
+        return res
