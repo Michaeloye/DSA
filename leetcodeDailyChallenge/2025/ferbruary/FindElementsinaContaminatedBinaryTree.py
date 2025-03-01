8f561b63546fc104000f917802dae9d2f06c3478
michaeloye <michaeloye23@gmail.com>
2025-03-02 00:19:40 +0100

    Add optimized solution

--- leetcodeDailyChallenge/2025/ferbruary/FindElementsinaContaminatedBinaryTree.py
+++ leetcodeDailyChallenge/2025/ferbruary/FindElementsinaContaminatedBinaryTree.py
@@ -38,3 +38,41 @@ class FindElements:
 # Your FindElements object will be instantiated and called as such:
 # obj = FindElements(root)
 # param_1 = obj.find(target)
+
+
+# OPTIMIZED SOLUTION
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.root.val = 0
+        self.seen = set([0])
+        self.resolveTree(self.root)
+    
+    def resolveTree(self, node):
+        if not node:
+            return
+        if node.left:
+            node.left.val = 2 * node.val + 1
+            self.seen.add(node.left.val)
+            self.resolveTree(node.left)
+        if node.right:
+            node.right.val = 2 * node.val + 2
+            self.seen.add(node.right.val)
+            self.resolveTree(node.right)
+        return
+    
+    def find(self, target: int) -> bool:
+        return target in self.seen
+    
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
