7a7c21ab8e0a90d1e28289fee61d71881eb89b73
michaeloye <michaeloye23@gmail.com>
2025-03-03 11:13:51 +0100

    Solve Partition Array According to Given Pivot

---
+++ leetcodeDailyChallenge/2025/march/PartitionArrayAccordingtoGivenPivot.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/partition-array-according-to-given-pivot/
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left = []
+        right = []
+        mid = []
+
+        for num in nums:
+            if num < pivot:
+                left.append(num)
+            elif num > pivot:
+                right.append(num)
+            else:
+                mid.append(num)
+        
+        return left + mid + right
