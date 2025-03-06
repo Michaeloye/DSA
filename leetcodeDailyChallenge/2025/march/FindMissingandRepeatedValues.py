ed9c859374a9a4aef930d99a35526068179a109b
michaeloye <michaeloye23@gmail.com>
2025-03-06 22:06:25 +0100

    Add another solution

--- leetcodeDailyChallenge/2025/march/FindMissingandRepeatedValues.py
+++ leetcodeDailyChallenge/2025/march/FindMissingandRepeatedValues.py
@@ -19,3 +19,24 @@ class Solution:
         nums = list(nums)
         ans[1] = nums[0]
         return ans
+
+# ANOTHER SOLUTION O(1) space
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        n = len(grid)
+
+        curSum = (1 + n * n) * (n * n)/2 # Sum of arithmetic progression (e, g 1 to 9)
+        seen = set()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                num = grid[r][c]
+                if num in seen:
+                    dup = num
+                else:
+                    seen.add(num)
+                    curSum -= num
+                
+        return [dup, int(curSum)]
