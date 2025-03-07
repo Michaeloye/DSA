4dbeb1b44969c15df20cb4c0b8f082db5b6d76f8
michaeloye <michaeloye23@gmail.com>
2025-03-07 13:14:45 +0100

    Add anotehr solution

--- leetcodeDailyChallenge/2025/march/ClosestPrimeNumbersinRange.py
+++ leetcodeDailyChallenge/2025/march/ClosestPrimeNumbersinRange.py
@@ -33,3 +33,35 @@ class Solution:
                 minDiff = diff
             
         return ans
+
+
+# Better solution
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        def isPrime(n):
+            if n <= 1:
+                return False
+            if n == 2:
+                return True
+            if n % 2 == 0:
+                return False
+            
+            for i in range(3, int(n ** 0.5) + 1, 2):
+                if n % i == 0:
+                    return False
+            return True
+        
+        prevPrime = -1
+        ans = [-1, -1]
+        minDiff = float('inf')
+
+        for num in range(left, right + 1):
+            if isPrime(num):
+                if prevPrime > 0:
+                    diff = num - prevPrime
+                    if diff < minDiff:
+                        ans = [prevPrime, num]
+                        minDiff = diff
+                prevPrime = num
+
+        return ans
