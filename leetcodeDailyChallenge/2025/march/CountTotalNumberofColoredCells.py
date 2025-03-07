f2df2e919686935c5b5404e54f27c346769e13c2
michaeloye <michaeloye23@gmail.com>
2025-03-07 01:40:19 +0100

    Add O(1) solution

--- leetcodeDailyChallenge/2025/march/CountTotalNumberofColoredCells.py
+++ leetcodeDailyChallenge/2025/march/CountTotalNumberofColoredCells.py
@@ -7,3 +7,9 @@ class Solution:
         for i in range(n):
             res += 4 * i
         return res
+
+# O(1) time solution
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        res = 1 + 4 * ((n - 1) * n //2)
+        return res
