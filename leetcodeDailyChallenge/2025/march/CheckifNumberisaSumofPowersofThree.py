6b0771b13bdff84fabf1fa7ba9ec48a4d53faa5b
michaeloye <michaeloye23@gmail.com>
2025-03-07 01:03:12 +0100

    Add optimal solution

--- leetcodeDailyChallenge/2025/march/CheckifNumberisaSumofPowersofThree.py
+++ leetcodeDailyChallenge/2025/march/CheckifNumberisaSumofPowersofThree.py
@@ -15,3 +15,23 @@ class Solution:
             return backtrack(i + 1, cur)
 
         return backtrack(0, 0)
+
+# optimal solution
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        i = 0
+
+        while 3 ** i <= n:
+            i += 1
+        
+        i -= 1
+
+        while i >= 0:
+            if 3 ** i > n:
+                i -= 1
+                continue
+            
+            n -= 3 ** i
+            i -= 1
+
+        return True if n == 0 else False
