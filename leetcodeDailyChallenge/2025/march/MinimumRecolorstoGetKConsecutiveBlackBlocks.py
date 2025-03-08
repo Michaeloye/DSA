acf11e650b0d42794a1939cd2d2cdf522ceac036
michaeloye <michaeloye23@gmail.com>
2025-03-08 01:43:28 +0100

    Solve Minimum Recolors to Get K Consecutive Black Blocks

---
+++ leetcodeDailyChallenge/2025/march/MinimumRecolorstoGetKConsecutiveBlackBlocks.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
+
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        wCount = 0
+        bCount = 0
+        ans = float('inf')
+        left  = 0
+
+        for right in range(len(blocks)):
+            if blocks[right] == 'W':
+                wCount += 1
+            else:
+                bCount += 1
+
+            if right - left + 1 == k:
+                # take count of "W" in that window, that's the number of Ws to change to black to have k consecutive
+                ans = min(ans, wCount)
+                if blocks[left] == 'W':
+                    wCount -= 1
+                else:
+                    bCount -= 1
+                left += 1
+        return ans
