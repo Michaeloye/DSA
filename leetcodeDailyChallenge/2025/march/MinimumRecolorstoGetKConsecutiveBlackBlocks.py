603b0d1386c19a26b88bcd80fe04d3236fb64b2a
michaeloye <michaeloye23@gmail.com>
2025-03-08 13:40:47 +0100

    Add solution for max recolors

--- leetcodeDailyChallenge/2025/march/MinimumRecolorstoGetKConsecutiveBlackBlocks.py
+++ leetcodeDailyChallenge/2025/march/MinimumRecolorstoGetKConsecutiveBlackBlocks.py
@@ -22,3 +22,26 @@ class Solution:
                     bCount -= 1
                 left += 1
         return ans
+
+# for max
+class Solution1:
+    def maximumRecolors(self, blocks: str, k: int) -> int:
+        wCount = 0
+        ans = 0
+        left = 0
+
+        for right in range(len(blocks)):
+            if blocks[right] == 'W':
+                wCount += 1
+            
+            if right - left + 1 == k:
+                ans = max(ans, wCount)
+
+                if blocks[left] == 'W':
+                    wCount -= 1
+                left += 1
+
+        return ans
+
+sol1 = Solution1()
+print(sol1.maximumRecolors("WBBWWBBWBW", 7))
