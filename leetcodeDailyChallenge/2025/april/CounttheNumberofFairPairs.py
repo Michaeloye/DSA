cc801c768585fcf1119cfb98206b804e35fdd720
michaeloye <michaeloye23@gmail.com>
2025-04-21 21:53:47 +0100

    Solve Count the Number of Fair Pairs

---
+++ leetcodeDailyChallenge/2025/april/CounttheNumberofFairPairs.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/count-the-number-of-fair-pairs/
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        def binarySearch(l, r, target):
+            while l <= r:
+                m  = l + (r - l) // 2
+                if nums[m] >= target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return r
+        res =0
+        nums.sort()
+        for i in range(len(nums)):
+            low = lower - nums[i]
+            up = upper - nums[i]
+
+            res += (
+                binarySearch(i + 1, len(nums) -1, up + 1) -
+                binarySearch(i + 1, len(nums) - 1, low)
+            ) 
+        
+        return res
