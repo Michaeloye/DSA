4d318cd6e37bbd8c344bcf3d5bfd98faea0fa67a
michaeloye <michaeloye23@gmail.com>
2025-05-02 15:33:26 +0100

    Solve Minimum Number of Operations to Make Elements in Array Distinct

---
+++ leetcodeDailyChallenge/2025/april/MinimumNumberofOperationstoMakeElementsinArrayDistinct.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        counter = {}
+        ans = 0
+
+        for i in range(len(nums)):
+            counter[nums[i]] = 1 + counter.get(nums[i], 0)
+        for k in range(len(nums)):
+            if k % 3 == 0:
+                if sum(counter.values()) == len(counter):
+                    return ans
+
+                ans += 1
+            counter[nums[k]] -= 1
+            if counter[nums[k]] == 0:
+                counter.pop(nums[k])
+
+        return ans
