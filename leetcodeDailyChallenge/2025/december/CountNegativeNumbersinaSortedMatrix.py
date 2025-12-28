20d6379ef2c2acb76d9a8540ff88464440ba10e6
michaeloye <michaeloye23@gmail.com>
2025-12-28 15:25:43 +0100

    Implement stair case method for counting negative numbers in a sorted matrix

--- leetcodeDailyChallenge/2025/december/CountNegativeNumbersinaSortedMatrix.py
+++ leetcodeDailyChallenge/2025/december/CountNegativeNumbersinaSortedMatrix.py
@@ -10,3 +10,22 @@ class Solution:
                     ans += 1
 
         return ans
+
+# STAIR CASE METHOD
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        ans = 0
+        n = len(grid)
+        m = len(grid[0])
+
+        i = 0
+        j = m - 1
+
+        while i < n and j >= 0:
+            if grid[i][j] < 0:
+                ans += n - i
+                j -= 1
+            else:
+                i += 1
+
+        return ans
