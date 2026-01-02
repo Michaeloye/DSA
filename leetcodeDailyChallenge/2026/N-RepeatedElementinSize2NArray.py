5ae0296fcb4626374adcef0dbe501a977f78c00e
michaeloye <michaeloye23@gmail.com>
2026-01-02 21:18:10 +0100

    Solve N-Repeated Element in Size 2N Array

---
+++ leetcodeDailyChallenge/2026/N-RepeatedElementinSize2NArray.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
+
+        return 0
